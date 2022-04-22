54cc48d20c93a22c907a9d2980a87a357f1ae95f
Carlos <carlos.karat@elev.ga.ntig.se>
2022-04-22 09:22:55 +0200

    urls views

--- EComSite/test1/views.py
+++ EComSite/test1/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Choice, Question
+from django.views import generic
+import requests
+import json
 
-# Create your views here.
